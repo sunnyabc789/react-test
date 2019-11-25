615e21f878cb9a62fbcd0380d002b384d970b145
sunnyabc789 <420095734@qq.com>
2019-11-25 17:59:55 +0800

    init-react

---
+++ src/entry/index.jsx
@@ -0,0 +1,10 @@
+import { render } from 'react-dom'
+import React from 'react'
+
+const rootElement = document.getElementById('container')
+
+console.log('here===')
+render (
+  <div>test</div>,
+  rootElement
+)
--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,27 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const os = require('os')
+const env = process.env.NODE_ENV || 'development'
+const HappyPack = require('happypack')
+
+packThreadCount = os.cpus().length - 1
 
 const extractTextPlugin = new ExtractTextPlugin({
   filename: 'css/[name].styles.css'
 })
 
+const happyThreadPool = packThreadCount === 0 ? null : HappyPack.ThreadPool({size: packThreadCount})
+
+const happyConf = {
+  loaders: ['babel-loader?cacheDirectory=true'],
+  threadPool: happyThreadPool,
+  verbose: true
+}
+
 module.exports = {
 	entry: {
-		app: './src/index.js'
+		app: './src/entry/index.jsx'
 	},
 	output: {
 		// filename: 'bundle.js',
@@ -17,8 +30,31 @@ module.exports = {
 		path: path.resolve(__dirname, 'dist')
   },
   devtool:"#eval-source-map",
+  devServer: {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+    },
+    historyApiFallback: true,
+    hot: true,
+    // open: true,
+    inline: true,
+    progress: true
+  },
   module: {
     rules: [
+      {
+        test: /\.worker\.js$/,
+        use: {loader: 'worker-loader'}
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: '/node_modules/',
+        use: [
+          packThreadCount === 0 ? 'babel-loader?cacheDirectory=true' : 'happypack/loader',
+          env === 'production' ? 'remove-debug-loader' : null
+        ].filter(x => x)
+      },
       {
         test: /\.css$/,
         //此时用的是use没用inculde exclude
@@ -81,8 +117,10 @@ module.exports = {
 	plugins: [
 		new CleanWebpackPlugin(['dist']),
 		new HtmlWebpackPlugin({
-			title: 'webpack4-splitchunk'
+      title: 'webpack4-splitchunk',
+      template: './view/index.html'
     }),
+    packThreadCount === 0 ? null : new HappyPack(happyConf),
     extractTextPlugin
   ]
 }
