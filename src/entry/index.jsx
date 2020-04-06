14c539230ce96b57e516328c199ab8dc884fee02
sunnyabc789 <420095734@qq.com>
2020-04-06 19:12:40 +0800

    save

--- src/entry/index.jsx
+++ src/entry/index.jsx
@@ -1,10 +1,10 @@
 import { render } from 'react-dom'
 import React from 'react'
-import App from '../components/main'
+import App from '../components/main/test-usecallback'
 
 const rootElement = document.getElementById('container')
 
 render (
-  <div>test</div>,
+  <App />,
   rootElement
 )
