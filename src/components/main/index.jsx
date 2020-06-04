e36c6f2942cdfd85f230618bce839716bab62ece
sunnyabc789 <420095734@qq.com>
2020-06-04 20:57:18 +0800

    save

--- src/components/main/index.jsx
+++ src/components/main/index.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 export default class App extends React.Component { 
 
+  tFun = () => {
+    console.log(this.props.setting, '2===')
+    return <div>test2</div>
+  }
   render() {
-    return <div>test</div>
+    console.log(this.props.setting, '1===')
+    return (
+      <div>test
+        {  this.tFun()}
+      </div>
+    )
   }
 }
--- src/entry/index.jsx
+++ src/entry/index.jsx
@@ -1,49 +1,14 @@
 import { render } from 'react-dom'
 import React from 'react'
-import { Tabs, WhiteSpace } from 'antd-mobile';
-import 'antd-mobile/dist/antd-mobile.css';
+import App from '../components/main'
 
 const rootElement = document.getElementById('container')
 
-const tabs = [
-  { title: 'First Tab' },
-  { title: 'Second Tab' },
-  { title: 'Third Tab' },
-];
-
-class App extends React.Component {
-
-  render() {
-
-    //antd-mobile Tabs有大坑  需要 
-    //1..am-tabs-tab-bar-wrap
-    //height 47px !important
-    //2. 行内 height:250px
-    return (
-      <div>
-        <WhiteSpace />
-        <div style={{ height: 200 }}>
-          <Tabs 
-            tabs={tabs}
-            initialPage={'t2'}
-          >
-            <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '250px', backgroundColor: '#fff' }}>
-              Content of first tab
-        </div>
-            <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '250px', backgroundColor: '#fff' }}>
-              Content of second tab
-        </div>
-            <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '250px', backgroundColor: '#fff' }}>
-              Content of third tab
-        </div>
-          </Tabs>
-        </div>
-      </div>
-    )
-  }
-}
-
 render (
-  <App />,
+  <App 
+    setting={{
+      test: 'abc'
+    }}
+  />,
   rootElement
 )
