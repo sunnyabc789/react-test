42d3ebebeb8151e311aece715c0ae064fe31587f
sunnyabc789 <420095734@qq.com>
2020-06-09 15:08:13 +0800

    save

--- src/components/main/index.jsx
+++ src/components/main/index.jsx
@@ -4,13 +4,14 @@ export default class App extends React.Component {
 
   tFun = () => {
     console.log(this.props.setting, '2===')
-    return <div>test2</div>
+    return <div  className="abc">test2</div>
   }
   render() {
     console.log(this.props.setting, '1===')
     return (
-      <div>test
+      <div id='test'>test
         {  this.tFun()}
+        <div>abc</div>
       </div>
     )
   }
--- src/entry/index.jsx
+++ src/entry/index.jsx
@@ -1,6 +1,7 @@
 import { render } from 'react-dom'
 import React from 'react'
 import App from '../components/main'
+import '../components/main/test.styl'
 
 const rootElement = document.getElementById('container')
 
